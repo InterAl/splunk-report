b5e600de537dc2a377e1db1b5500ec03c3ccbd51
InterAl <interal@gmail.com>
2015-09-23 13:40:01 +0300

    toDate is set to midnight. more billing events.

--- app.js
+++ app.js
@@ -3,11 +3,9 @@
 var fromDate = new Date(process.argv[2]);
 var toDate = new Date(process.argv[3]);
 
-if (toDate.toISOString() == fromDate.toISOString()) {
-    toDate.setHours(23);
-    toDate.setMinutes(59);
-    toDate.setSeconds(59);
-}
+toDate.setHours(23);
+toDate.setMinutes(59);
+toDate.setSeconds(59);
 
 errorReportGen(fromDate, toDate).then(function(f) {
     console.log("Done!");
